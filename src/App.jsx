35a295042baf23c80096b811054b840a48903245
sanggusseu <sanggusseu@gmail.com>
2024-09-24 11:35:21 +0900

    Prettier & ESLint 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-export default function App(){
-    return(
-        <div>
-<h1>hi</h1>
-        </div>
-    );
+export default function App() {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
 }
