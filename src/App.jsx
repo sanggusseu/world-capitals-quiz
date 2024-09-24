88ca8e5427f5fa37c3559a609c059861c0e1e2a0
sanggusseu <sanggusseu@gmail.com>
2024-09-24 11:58:43 +0900

    페이지 경로 & 에러 페이지 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <header>
+        <h1>Hi</h1>
+      </header>
+      <Outlet />
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,30 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ExplorePage from './pages/ExplorePage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import QuizPage from './pages/QuizPage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: 'explore',
+        element: <ExplorePage />,
+      },
+      {
+        path: 'quiz',
+        element: <QuizPage />,
+      },
+    ],
   },
 ]);
 
