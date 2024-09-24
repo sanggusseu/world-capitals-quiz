ba5d9f1538ee4d821be2a41d288b8230a8a26269
sanggusseu <sanggusseu@gmail.com>
2024-09-24 20:37:49 +0900

    배경 이미지 설정 및 버튼 디자인 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,34 +2,26 @@ import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <section className="flex flex-col items-center h-screen">
+    <section className="flex flex-col items-center">
       <h1 className="sr-only">홈페이지</h1>
-      <div>
-        {/* TODO: 이미지 설정하기 */}
-        <img src="" alt="" />
-      </div>
-
-      <div className="inline-flex flex-col gap-y-3">
-        <Link
-          className="group relative inline-block focus:outline-none focus:ring"
-          to="/explore"
-        >
-          <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
-
-          <span className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
-            둘러보기
-          </span>
-        </Link>
-        <Link
-          className="group relative inline-block focus:outline-none focus:ring"
-          to="/quiz"
-        >
-          <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
-
-          <span className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
-            퀴즈풀기
-          </span>
-        </Link>
+      <div className="home-bg w-full relative h-[70vh]">
+        <div className="absolute right-[50%] translate-x-[50%] bottom-10 min-w-32 inline-flex flex-col gap-y-3">
+          <Link className="relative" to="/explore">
+            <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+            <span class="fold-bold text-center relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+              둘러보기
+            </span>
+          </Link>
+          <Link
+            className="group relative inline-block focus:outline-none focus:ring"
+            to="/quiz"
+          >
+            <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+            <span class="fold-bold text-center relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+              퀴즈풀기
+            </span>
+          </Link>
+        </div>
       </div>
     </section>
   );
