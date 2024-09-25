c95df45e4c87dcc1cc18eff3a86fdec6e583014f
sanggusseu <sanggusseu@gmail.com>
2024-09-25 12:50:01 +0900

    Kakao Adfit 설정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+
 export default function Footer() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <footer className="fixed bottom-0 bg-gray-800 w-full min-h-[100px]">
       <ins
