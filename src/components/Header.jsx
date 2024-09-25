42e26ead9d5322c3fc0c2c9b52b9786e6c6a79c6
sanggusseu <sanggusseu@gmail.com>
2024-09-25 11:08:52 +0900

    Header fixed 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   };
   return (
     <>
-      <header className="bg-gray-800 text-white">
+      <header className="fixed top-0 left-0 right-0 bg-gray-800 text-white">
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="flex-1 md:flex md:items-center md:gap-12">
